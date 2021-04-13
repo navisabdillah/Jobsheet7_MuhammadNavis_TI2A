544767c2014423fe7f934d442324a27245a6be06
= <=>
2021-04-13 10:06:56 +0700

    Latihan Praktikum 9: Membuat relasi many to many

--- resources/views/mahasiswas/index.blade.php
+++ resources/views/mahasiswas/index.blade.php
@@ -60,7 +60,7 @@
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="btn btn-danger">Delete</button>
-                
+                <a class="btn btn-warning" href="{{ route('mahasiswas.showNilai', $Mahasiswa->Nim) }}">Nilai</a>
             </form>
         </td>
     </tr>
